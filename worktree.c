f77159a1f0ce8038aa2014d5374f09070c56bde7
Julien <julienremant95@Gmail.com>
2023-03-23 17:37:47 +0100

    Update worktree.c
    Toutes les fonctions y sont

--- worktree.c
+++ worktree.c
@@ -5,8 +5,9 @@
 #include <sys/stat.h>
 #include "worktree.h"
 #include "hachage.h"
+#include "directory.h"
 
-#define TAILLE 3
+#define TAILLE 10
 
 WorkFile* createWorkFile(char* name){
     //Alloc
@@ -93,7 +94,6 @@ int appendWorkTree(WorkTree* wt, char* name, char* hash, int mode){
     }
     return -1;
 }
-
 
 char* wtts(WorkTree* wt){
     //Alloc
@@ -213,16 +213,18 @@ char* blobWorkTree(WorkTree* wt){
     //création du fichier temporaire
     char tmp[100];
     char* filename = (char*)malloc(100*sizeof(char));
-    strcpy(filename,"testXXXXXX"); 
-    sprintf(tmp, "tmp/%sXXXXXX", filename);
+    strcpy(filename,"test"); 
+    sprintf(tmp, "%sXXXXXX", filename);
     int id=mkstemp(tmp);
     if(id == -1){
         printf("Création du fichier tmp echoué\n");
+        return NULL;
     }
 
     //écriture du contenu dans le fichier tmp
     if(wttf(wt, tmp) == -1){
         printf("échec dans l'écriture\n");
+        return NULL;
     }
 
     //hachage du contenu de tmp
@@ -278,25 +280,31 @@ char* saveWorkTree(WorkTree* wt, char* path){
         char name[100];
         sprintf(name, "%s%s", path, tab[i].name);
         stat(name, ret);
+        // test si c'est un fichier
         if(S_ISREG(ret->st_mode)){
+            // le cas où c'est un fichier
             blobFile(name);
             tab[i].mode = getChmod(name);
+            // peut être rajouté un if(tab[i].hash) avant de le free.
             free(tab[i].hash);
             tab[i].hash = sha256file(name);
         }
         else{
+            // le cas où c'est un répértoire
             WorkTree* newWT = initWorkTree();
             strcat(name, "/");
             List* L = listdir(name);
             Cell* C = *L;
             while(C){
-                appendWorkTree(newWT, C->data, "(null)", 0);
+                appendWorkTree(newWT, C->data, "(null)", getChmod(C->data));
                 C = C->next;
             }
             char* s = saveWorkTree(newWT, name);
-            tab[i].mode = getChmod(name);
+            // peut être rajouté un if(tab[i].hash) avant de le free.
             free(tab[i].hash);
+            tab[i].mode = getChmod(name);
             tab[i].hash = blobWorkTree(newWT);
+            
             free(s);
             libererWorkTree(newWT);
             libererList(L); 
@@ -307,6 +315,71 @@ char* saveWorkTree(WorkTree* wt, char* path){
     char* hash = blobWorkTree(wt);
     return hash;    
 }
+
+void setMode ( int mode , char * path ){
+    char buff [100];
+    sprintf(buff ,"chmod %d %s", mode ,path);
+    system (buff);
+}
+
+void restoreWorkTree(WorkTree* wt, char* path){
+    WorkFile* tab = wt->tab;
+    int i;
+    int fic[2] = {0,0};
+    for(i=0; i<wt->n; i++){
+        fic[0] = 0;
+        fic[1] = 0;
+        char* chemin = hashToPath(tab[i].hash);
+
+        // Test d'existence de l'enregistrement instantané
+        FILE* f = fopen(chemin, "r");
+        if(f) fic[0] = 1;
+        char buffer[120];
+        strcpy(buffer, chemin);
+        strcat(buffer, ".t");
+        FILE* t = fopen(buffer, "r");
+        if(t) fic[1] = 1;
+
+        // Si le fichier n'existe pas
+        if((fic[0] == 0) && (fic[1] == 0)){
+            printf("L'enregistrement instantané n'existe pas %s\n",chemin);
+            return;
+        }
+
+        // S'il en existe deux
+        if((fic[0] == 1) && (fic[1] == 1)){
+            printf("Collision de l'enregistrement instantané %s\n",chemin);
+            fclose(f);
+            fclose(t);
+            return;
+        }
+
+        // Dans ce cas on est dans un fichier
+        if((fic[0] == 1) && (fic[1] == 0)){
+            char template[150];
+            sprintf(template, "%s%s", path, tab[i].name);
+            cp(chemin, template);
+            setMode(tab[i].mode, template);
+            fclose(f);
+            free(chemin);
+        }
+
+       // Dans ce cas on est dans un répértoire
+       if((fic[0] == 0) && (fic[1] == 1)){
+            WorkTree* NewWT = ftwt(buffer);
+            char template[150];
+            sprintf(template, "%s%s/", path, tab[i].name);
+            restoreWorkTree(NewWT, template);
+            fclose(t);
+            free(chemin);
+            libererWorkTree(NewWT);
+        }
+
+     }
+
+
+}
+
 
 int main(){
     //createWorkFile
@@ -353,18 +426,18 @@ int main(){
     printf("Le contenu de wt3 est :\n%s", s4);    
 
     //blobWorkTree
-    char* s5 = blobWorkTree(wt4);
+    //char* s5 = blobWorkTree(wt4);
 
     //saveWorkTree
     WorkTree* wt4 = initWorkTree();
     // Pour pouvoir tester cette fonction il faudra créer au préalable
     // un répértoire exercice5 qui contient:
     // fichier1.txt , exercice1 et fichier2.txt
-    appendWorkTree(wt4, "exercice1", "test1", 0); // ici les hash et le mode on met
+    appendWorkTree(wt4, "fichier2.txt", "test1", 0); // ici les hash et le mode on met
     appendWorkTree(wt4, "exercice1", "test1", 0);   // n'importe quoi car on va les 
     appendWorkTree(wt4, "fichier1.txt", "test1", 0); // modifier par la suite
+    char* s6= saveWorkTree(wt4, "exercice5/");
     
-
 
 
     libererWorkFile(wf1);
@@ -372,11 +445,14 @@ int main(){
     libererWorkTree(wt1);
     libererWorkTree(wt2);
     libererWorkTree(wt3);
-    libererWorkTree(wt4);
+    //libererWorkTree(wt4);
     free(s1);
     free(s2);
     free(s3);
     free(s4);
-    free(s5);
+    //free(s5);
     free(s6);
 }
+
+
+
